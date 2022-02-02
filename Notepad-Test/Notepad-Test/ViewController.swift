d237fa46a5ae491b612c26aa8c8846d2ee71d27f
Lagrange1813 <Lagrange1813@163.com>
2022-02-02 21:20:12 +0800

    2022.2.2

---
+++ Notepad-Test/Notepad-Test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Notepad-Test
+//
+//  Created by 张维熙 on 2022/2/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var test: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+    }
+
+
+}
+
